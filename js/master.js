751b28ef266a0c19211e535610e8e924aff3db9b
Daniel Fuentes <=>
2022-02-23 20:03:27 -0300

    Actualización del código javascript - condición

--- js/master.js
+++ js/master.js
@@ -1,4 +1,8 @@
 let usuario = document.getElementById('usuario');
 let nombre = prompt('Indique su nombre');
+if(nombre === ''){
+    usuario.innerHTML = `Bienvenid@ a esta gran aventura Invitado`;
+}else{
+    usuario.innerHTML = `Bienvenid@ a esta gran aventura ${nombre}`;
+}
 
-usuario.innerHTML = `Bienvenid@ a esta gran aventura ${nombre}`;
